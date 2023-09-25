444e52d7b5051e1afa11ac1159778d357e1e6634
Bamdyl <mmorawski520@gmail.com>
2023-09-25 09:52:18 +0200

    prepare methods

---
+++ app222/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  app222
+//
+//  Created by Student-02 on 25/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    var blur: CGFloat;
+    var sepia: CGFloat;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func setBlur(radius: CGFloat) -> UIImage?{
+       
+    }
+    
+    func setSepia(radius: CGFloat) -> UIImage?{
+        
+    }
+}
+
